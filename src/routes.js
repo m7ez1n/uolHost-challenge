712b7fdc416c011fd190cc61991b8483012dead0
m7Aei <mateuspm2015@hotmail.com>
2019-10-13 16:17:27 -0300

    Importando a rota da pagina home

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-// importar páginas depois
+import Home from './pages/Home';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path='caminho pra colocar' exact component={/** nome da pagina */} />
+      <Route path="/" exact component={Home} />
     </Switch>
   );
 }
