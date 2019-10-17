fc7ea79630e92290034d0aa814efd2a7a079b6cf
m7Aei <mateuspm2015@hotmail.com>
2019-10-16 23:32:21 -0300

    Corrigindo a rota para criar um novo cliente

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import NewClient from './pages/NewClient';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/client" exact />
+      <Route path="/clients" exact component={NewClient} />
     </Switch>
   );
 }
